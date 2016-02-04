42771660691a7dd42f56823314d6f96bf34f5100
Marko Tiikkaja <marko@joh.to>
2016-02-04 21:58:55 +0100

    postgres-strict: Disable extra violation checks by default
    I couldn't see an easy way to pass a different default to all the
    different tests in the source tree.  If there is one, I'd rather revert
    this and do that instead.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -3450,7 +3450,7 @@ static struct config_string ConfigureNamesString[] =
 			GUC_LIST_INPUT
 		},
 		&postgres_strict_disable_string,
-		"none",
+		"all",
 		check_postgres_strict_disable, assign_postgres_strict_disable, NULL
 	},
 
