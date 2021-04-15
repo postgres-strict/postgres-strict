fb7b80b42b987210127a1954c0ce90d3771e3c61
Marko Tiikkaja <marko@joh.to>
2021-04-16 02:03:26 +0300

    postgres-strict: Add GUC for changing the emitted error level
    There's also a special "assert" option which uses the same SQLSTATE as
    ASSERT in PL/PgSQL; this is useful because such an exception can not be
    caught in PL/PgSQL.  For all other options the SQLSTATE is
    ERRCODE_POSTGRES_STRICT_VIOLATION (PS001).

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -56,6 +56,7 @@
 #include "parser/parser.h"
 #include "parser/scansup.h"
 #include "pgstat.h"
+#include "postgres_strict.h"
 #include "postmaster/autovacuum.h"
 #include "postmaster/bgworker_internals.h"
 #include "postmaster/bgwriter.h"
@@ -194,6 +195,7 @@ static bool check_cluster_name(char **newval, void **extra, GucSource source);
 static const char *show_unix_socket_permissions(void);
 static const char *show_log_file_mode(void);
 static const char *show_data_directory_mode(void);
+static void assign_postgres_strict_violation_level(int newval, void *extra);
 
 /* Private functions in guc-file.l that need to be called from guc.c */
 static ConfigVariable *ProcessConfigFileInternal(GucContext context,
@@ -421,6 +423,14 @@ static const struct config_enum_entry password_encryption_options[] = {
 	{NULL, 0, false}
 };
 
+static const struct config_enum_entry postgres_strict_violation_level_options[] = {
+	{"warning", WARNING, false},
+	{"error", ERROR, false},
+	{"fatal", FATAL, false},
+	{"assert", POSTGRES_STRICT_ASSERT, false},
+	{NULL, 0, false}
+};
+
 /*
  * Options for enum values stored in other modules
  */
@@ -490,6 +500,9 @@ int			ssl_renegotiation_limit;
  */
 int			huge_pages;
 
+int postgres_strict_violation_level;
+int postgres_strict_violation_sqlstate = ERRCODE_POSTGRES_STRICT_VIOLATION;
+
 /*
  * These variables are all dummies that don't do anything, except in some
  * cases provide the value for SHOW to display.  The real state is elsewhere
@@ -520,6 +533,7 @@ static bool data_checksums;
 static bool integer_datetimes;
 static bool assert_enabled;
 static char *postgres_strict_disable_string;
+static int postgres_strict_violation_level_enum;
 
 /* should be static, but commands/variable.c needs to get at this */
 char	   *role_string;
@@ -4169,6 +4183,16 @@ static struct config_enum ConfigureNamesEnum[] =
 		&Password_encryption,
 		PASSWORD_TYPE_MD5, password_encryption_options,
 		NULL, NULL, NULL
+	},
+
+	{
+		{"plpgsql_strict.violation_level", PGC_USERSET, DEVELOPER_OPTIONS,
+			gettext_noop("Sets the level used for reporting violations specific to postgres-strict."),
+			NULL
+		},
+		&postgres_strict_violation_level_enum,
+		ERROR, postgres_strict_violation_level_options,
+		NULL, assign_postgres_strict_violation_level, NULL
 	},
 
 	/* End-of-list marker */
@@ -10906,6 +10930,29 @@ show_data_directory_mode(void)
 
 	snprintf(buf, sizeof(buf), "%04o", data_directory_mode);
 	return buf;
+}
+
+static void
+assign_postgres_strict_violation_level(int newval, void *extra)
+{
+	switch (newval)
+	{
+		case WARNING:
+		case ERROR:
+		case FATAL:
+			postgres_strict_violation_level = newval;
+			postgres_strict_violation_sqlstate = ERRCODE_POSTGRES_STRICT_VIOLATION;
+			break;
+
+		case POSTGRES_STRICT_ASSERT:
+			postgres_strict_violation_level = ERROR;
+			postgres_strict_violation_sqlstate = ERRCODE_ASSERT_FAILURE;
+			break;
+
+		default:
+			/* shouldn't happen */
+			elog(ERROR, "unrecognized value %d", newval);
+	}
 }
 
 #include "guc-file.c"
